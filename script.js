228c9da13b85d18c15e6066837052c450878d5a3
M-Stasiak <mateusz.stasiak16@gmail.com>
2023-08-12 00:54:29 +0200

    New photo effects and Social Media Bar with Effects

---
+++ script.js
@@ -0,0 +1,19 @@
+/*const photo = document.querySelector("img");
+
+var reverse = true;
+let pom = 0;
+function animate() {
+    reverse ? pom += 2 : pom -= 2;
+
+    if (pom >= 1000) {
+        reverse = false
+    }
+    if (pom <= 0) {
+        reverse = true
+    }
+
+    photo.style.margin = pom + 'px';
+    requestAnimationFrame(animate); // Wywołaj ponownie funkcję animate
+  }
+
+  animate(); // Rozpocznij animację po załadowaniu strony*/
